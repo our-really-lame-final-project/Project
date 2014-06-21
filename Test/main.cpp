b26493599770d612aa1cd5234bd8ed75d2291c04
Kevinortiz95 <kevinortiz95@outlook.com>
2014-06-21 12:59:15 -0700

    Fixed display error

--- Test/main.cpp
+++ Test/main.cpp
@@ -8,26 +8,25 @@
 
 int main()
 {
-    ALLEGRO_DISPLAY *display;
-
-    if(al_init())
+    if(!al_init())
     {
         al_show_native_message_box(NULL, NULL, "Error", "Could not initialize Allegro 5", NULL, ALLEGRO_MESSAGEBOX_ERROR);
         return -1;
     }
-    display = al_create_display(800, 600);
 
-    if(!display)
-    {
-        al_show_native_messagebox(NULL, NULL, "Error", "Could not create Allegro 5 display", NULL, ALLEGRO_MESSAGEBOX_ERROR);
-        return -1;
-    }
 
     // You generally want to do this after you check to see if the display was created. If the display wasn't created then there's
     // no point in calling this function
-    al_set_new_display_flags(ALLEGRO_NOFRAME);
+    //al_set_new_display_flags(ALLEGRO_NOFRAME);
+    ALLEGRO_DISPLAY *display = al_create_display(800, 600);
     al_set_window_position(display, 200, 100);
     al_set_window_title(display, "CodingMadeEasy");
+
+    if(!display)
+    {
+        al_show_native_message_box(NULL, NULL, "Error", "Could not create Allegro 5 display", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
 
     al_rest(5.0);
     al_destroy_display(display);
