4f3860694dbb6607724e2f65148a079cce951d47
Kevinortiz95 <kevinortiz95@outlook.com>
2014-06-22 20:53:39 -0700

    side scrolling, collisions

--- Test/main.cpp
+++ Test/main.cpp
@@ -6,11 +6,29 @@
 #include<allegro5/allegro_primitives.h>//to draw images.
 #include<allegro5/allegro_audio.h>//Play audio.
 #include<allegro5/allegro_acodec.h>//Play different file types for audio.
+#include<cmath>
 
 #define ScreenWidth 800//sets screen width variable.
 #define ScreenHeight 600//sets screen height variable.
 
 //When using pointers(*) you need to make sure to destroy them before you run and build the program.
+void CameraUpdate(float *cameraPosition, float x, float y, int width, int height)
+{
+    cameraPosition[0] = -(ScreenWidth / 2) + (x + width / 2);
+    cameraPosition[1] = -(ScreenHeight / 2) + (y + height / 2);
+    if(cameraPosition[0] < 0)
+        cameraPosition[0] = 0;
+    if(cameraPosition[1] < 0)
+        cameraPosition[1] = 0;
+}
+bool Collision(float x, float y, float ex, float ey, int pradius, int eradius)
+{//Pythagoream theorem: using hypontenuse to find distance between objects.
+    if(sqrt(pow(x - ex, 2) + pow(y - ey, 2)) < pradius + eradius)
+    {
+        return true;//Collision.
+    }
+    return false;//No collision.
+}
 
 int main()
 {
@@ -39,11 +57,16 @@ int main()
     float x = 10, y = 10, moveSpeed = 5;//sets player position and speed.
     int dir = DOWN;//Sets players direction.
     int sourceX = 32, sourceY = 0;//Standing position.
-    float velx, vely; //Changes jump speed with gravity.
+
+    float cameraPosition[2] = {0, 0};
+    /*------------------------------------------------------------------------------------*/
+    //Gravity code.
+    /*float velx, vely; //Changes jump speed with gravity.
     velx = vely = 0;//Set default value .
     bool jump = false;//Sets jump loop to false.
     float jumpSpeed = 15;//Sets players jump speed.
-    const float gravity = 1;//Sets realistic gravity settings.
+    const float gravity = 1;//Sets realistic gravity settings.*/
+    /*------------------------------------------------------------------------------------*/
 
     al_init_font_addon();//initialize font addon.
     al_init_ttf_addon();//initialize ttf files.
@@ -63,9 +86,11 @@ int main()
     al_set_sample_instance_playmode(songInstance, ALLEGRO_PLAYMODE_LOOP);//Choose play mode for song like looping the song.
     al_attach_sample_instance_to_mixer(songInstance, al_get_default_mixer());//Attach the song to the default mixer.
 
-    ALLEGRO_BITMAP *player = al_load_bitmap("Test.png");//Creates bitmap.
+    ALLEGRO_BITMAP *player = al_load_bitmap("Test.png");//Creates bitmap for player.
+    ALLEGRO_BITMAP *background = al_load_bitmap("background.png");//Creates an object.
 
     ALLEGRO_KEYBOARD_STATE keyState;//Tells the program what key was pressed.
+    ALLEGRO_TRANSFORM camera;
 
     ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);//Set timer to 60 fps.
     ALLEGRO_TIMER *frameTimer = al_create_timer(1.0 / frameFPS);//Set timer to 15 fps.
@@ -78,7 +103,7 @@ int main()
 
     al_play_sample_instance(songInstance);//Starts the song.
     al_flip_display();//shows the font.
-    al_rest(4.0);//sets screen timer.
+    al_rest(1.0);//sets screen timer to 4.0.
 
     al_start_timer(timer);//Starts the timer.
     al_start_timer(frameTimer);
@@ -109,7 +134,7 @@ int main()
             if(events.timer.source == timer)
             {
                 active = true;
-               /* if(al_key_down(&keyState, ALLEGRO_KEY_DOWN))
+                if(al_key_down(&keyState, ALLEGRO_KEY_DOWN))
                 {
                     y += moveSpeed;
                     dir = DOWN;
@@ -120,8 +145,40 @@ int main()
                 {
                     y -= moveSpeed;
                     dir = UP;
-                }*/
-                if(al_key_down(&keyState, ALLEGRO_KEY_RIGHT))
+                }
+                else if(al_key_down(&keyState, ALLEGRO_KEY_RIGHT))
+                {
+                    x += moveSpeed;
+                    dir = RIGHT;
+                }
+                else if(al_key_down(&keyState, ALLEGRO_KEY_LEFT))
+                {
+                    x -= moveSpeed;
+                    dir = LEFT;
+                }
+                else
+                    active = false;
+
+                CameraUpdate(cameraPosition, x, y, 32, 32);
+
+                al_identity_transform(&camera);
+                al_translate_transform(&camera, -cameraPosition[0], -cameraPosition[1]);
+                al_use_transform(&camera);
+
+                if(Collision(x, y, 200, 200, 10, 20))
+                {//If collision true, run if statement.
+                    if(dir == 0)
+                        y -= moveSpeed;
+                    else if(dir == 1)
+                        x += moveSpeed;
+                    else if(dir == 2)
+                        x -= moveSpeed;
+                    else if(dir == 3)
+                        y += moveSpeed;
+                }
+                /*------------------------------------------------------------------------------------*/
+                //Gravity code.
+                /*if(al_key_down(&keyState, ALLEGRO_KEY_RIGHT))
                 {
                     velx = moveSpeed;
                     dir = RIGHT;
@@ -140,8 +197,10 @@ int main()
                 {
                     vely = -jumpSpeed;
                     jump = false;
-                }
+                }*/
+                /*------------------------------------------------------------------------------------*/
             }
+
             else if (events.timer.source == frameTimer)
             {
                 if(active)
@@ -154,8 +213,11 @@ int main()
 
                 sourceY = dir;//Direction of player.
             }
-
-            if(!jump)
+            draw = true;//If any keys are used, them draw will return true and draw the image.
+        }
+            /*------------------------------------------------------------------------------------*/
+            //Gravity code.
+            /*if(!jump)
                 vely += gravity; //Sets gravity active for jump..
             else
                 vely = 0;//Sets gravity to zero.
@@ -166,19 +228,21 @@ int main()
             jump = (y + 32 >= 600);//Sets jump border for player.
 
             if(jump)
-                y = 600 - 32;//Places players height into correct position.
-
-            draw = true;//If any keys are used, them draw will return true and draw the image.
-        }
+                y = 568;//Places players height into correct position.*/
+                /*------------------------------------------------------------------------------------*/
 
         if(draw)//Draws the image when keys are inputted.
         {
-            draw = false;
-            al_draw_bitmap_region(player, sourceX, sourceY * al_get_bitmap_height(player) / 4, 32, 32, x, y, NULL);//Draws the bitmap animation on screen.
+            ALLEGRO_BITMAP *subBitmap = al_create_sub_bitmap(player, sourceX, sourceY * 32, 32, 32);
+            al_draw_bitmap(background, 0, 0, NULL);
+            al_draw_bitmap(subBitmap, x, y, NULL);
+            //al_draw_bitmap_region(player, sourceX, sourceY * al_get_bitmap_height(player) / 4, 32, 32, x, y, NULL);//Draws the bitmap animation on screen.
+            //al_draw_bitmap_region(player2, 0, 0, 32, 32, 200, 200, NULL);//Draws the bitmap on the screen.
             //al_draw_bitmap(player, x, y + 440, NULL);//Draws the bitmap on screen.
             //al_draw_rectangle(x, y, x + 10, y + 10, al_map_rgb(44, 117, 255), 1);//Draws rectangle.
             al_flip_display();//Displays the image.
             al_clear_to_color(al_map_rgb(0, 0, 0));//Set background color.
+            al_destroy_bitmap(subBitmap);
         }
     }
 
@@ -192,6 +256,7 @@ int main()
     al_destroy_display(display);//destroy display.
     al_destroy_timer(timer);//destroy timer.
     al_destroy_bitmap(player);//Destroy play bitmap.
+    al_destroy_bitmap(background);//Destroy play bitmap.
     al_destroy_sample(soundEffect);//Destroy sound.
     al_destroy_sample(song);//Destroy song.
     al_destroy_sample_instance(songInstance);//Destroy song instance.
