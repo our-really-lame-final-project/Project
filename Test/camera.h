cc0a25b65daa22794ccf4b1c43170e3baa46c025
Michael Ackerman <binarymac@gmail.com>
2014-06-25 08:32:35 -0700

    comment

--- Test/camera.h
+++ Test/camera.h
@@ -2,6 +2,7 @@ void CameraUpdate(float *cameraPosition, int screenw, int screenh, float x, floa
 {
     // x & y are the player pos. + height and width of char cuz the player pos
     // is the top left corner of box
+    // screen / 2 to get to the middle
     cameraPosition[0] = -(screenw/ 2.0) + (x + width / 2); 
     cameraPosition[1] = -(screenh / 2.0) + (y + height / 2);
     if (cameraPosition[0] < 0)
