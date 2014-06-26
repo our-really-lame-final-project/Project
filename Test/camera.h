e662d8d86233b0400017863cdd545559be0f8b9a
cheezitzrice <cheezitzrice@gmail.com>
2014-06-25 17:31:30 -0700

    Camera & Collision.h Redux
    Commented and slightly cleaned.

--- Test/camera.h
+++ Test/camera.h
@@ -1,10 +1,14 @@
+// FUNCTION FOR UPDATING CAMERA POSITION.
+/* ---------------------------------------------------------------------------------------------------------------------- */
+
 void CameraUpdate(float *cameraPosition, int screenw, int screenh, float x, float y, int width, int height)
 {
-    // x & y are the player pos. + height and width of char cuz the player pos
-    // is the top left corner of box
-    // screen / 2 to get to the middle
+    // x & y are the player position plus height and width of char because the player position
+    // is the top left corner of box screen, divide by 2 to get to the middle.
     cameraPosition[0] = -(screenw/ 2.0) + (x + width / 2);
     cameraPosition[1] = -(screenh / 2.0) + (y + height / 2);
+
+    // The following if statements are for camera boundaries. It prevents the camera from leaving bounded regions.
     if (cameraPosition[0] < 0)
     {
         cameraPosition[0] = 0;
--- Test/collision.h
+++ Test/collision.h
@@ -1,24 +1,45 @@
+// COLLISION BOOLEAN.
+/* ---------------------------------------------------------------------------------------------------------------------- */
+
 bool Collision(float x, float y, float ex, float ey, int width, int height)
 {
+    // A if statement for checking player's location to a restricted location.
     if(x + width < ex || x > ex + width || y + height < ey || y > ey + height)
     {
+        // False, since player is not in a restricted zone.
         return false;
     }
+
+    // Then return true if ever.
     return true;
 }
 
+// OBSTRUCT / REPEL FUNCTION.
+/* ---------------------------------------------------------------------------------------------------------------------- */
+
 float obstruct(float& x, float& y, int dir, float moveSpeed)
 {
+    // If a player / npc is in a restricted location, repel them the opposite direction.
     // from the top
     if(dir == 0)
+    {
         y -= moveSpeed;
+    }
     // if coming from the right
+
     else if(dir == 1)
+    {
         x += moveSpeed;
+    }
+
     // if coming from the left
     else if(dir == 2)
+    {
         x -= moveSpeed;
+    }
     // if coming from the bottom
     else if(dir == 3)
+    {
         y += moveSpeed;
+    }
 }
